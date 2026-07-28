679567267996fabdd8bd9803df813a24d68eb233
Shein-Alexander013 <78673939+Shein-Alexander013@users.noreply.github.com>
2026-07-28 16:49:30 +0700

    feat: Configure CORS and environment example

---
+++ config/cors.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => [
+        'GET',
+        'POST',
+        'OPTIONS',
+    ],
+
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => [
+        '*',
+    ],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
