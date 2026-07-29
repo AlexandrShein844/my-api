754fab5962eae98865bcac93102f6d9d12985f36
Shein-Alexander013 <78673939+Shein-Alexander013@users.noreply.github.com>
2026-07-29 18:52:18 +0700

    feat: Updare CORS config

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,10 @@ return [
         'OPTIONS',
     ],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
-    ],
-
+    'allowed_origins' => array_filter([
+        'http://localhost:5173',
+        env('FRONTEND_URL'),
+    ]),
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => [
