64162c67dc6010e7d00428e77f45029e19d79e24
Shein-Alexander013 <78673939+Shein-Alexander013@users.noreply.github.com>
2026-07-29 22:54:49 +0700

    fix: Update swagger config

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -17,8 +17,9 @@ return [
             'paths' => [
                 /*
                  * Edit to include full URL in ui for assets
+                 * Use relative URLs on HTTPS hosts to avoid mixed content issues.
                  */
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
+                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', false),
 
                 /*
                 * Edit to set path where swagger ui assets should be stored
@@ -117,8 +118,8 @@ return [
              * @link https://zircote.github.io/swagger-php/reference/processors.html
              */
             'default_processors_configuration' => [
-            /** Example */
-            /**
+                /** Example */
+                /**
              * 'operationId.hash' => true,
              * 'pathFilter' => [
              * 'tags' => [
@@ -229,8 +230,7 @@ return [
                     'name' => 'Authorization', // The name of the header or query parameter to be used.
                     'in' => 'header', // The location of the API key. Valid values are "query" or "header".
                 ],
-                */
-            ],
+                */],
             'security' => [
                 /*
                  * Examples of Securities
@@ -243,8 +243,7 @@ return [
                     ],
 
                     'passport' => []
-                    */
-                ],
+                    */],
             ],
         ],
 
